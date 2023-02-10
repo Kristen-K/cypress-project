203ee784e34d77511b2262489a52a2a8067222dd
Kristen-K <42575870+Kristen-K@users.noreply.github.com>
2023-02-10 21:49:06 +0000

    Added post length test

--- cypress/e2e/blog.cy.js
+++ cypress/e2e/blog.cy.js
@@ -1,8 +1,36 @@
 describe("Get list of recent posts, check they are >10 characters & total of 5 posts", () => {
-    context("Blog page", () => {
+
+    beforeEach(() => {
+        cy.visit("https://practice.automationbro.com/blog/")
+    })
+    context("Blog page: number of posts", () => {
         it("Get list of recent posts", () => {
-            cy.visit("https://practice.automationbro.com/blog/")
             cy.get("#recent-posts-3 ul li").should("have.length", 5)
+
         })
+    })
+
+    context("Blog page: number of posts", () => {
+        it("length of post should be greather than 10", () => {
+            cy.get("#recent-posts-3 ul li").then(($list) => {
+                for (var i = 0; i < $list.length; i ++) {
+                     cy.get($list[i]).invoke('text').then((text) => {
+                        expect(text.length).to.be.gte(10)
+                        cy.log(text)
+                     })
+                    
+                     //works:
+                     //expect(text.length).to.be.at.least(10)
+                
+
+                    //doesn't work:
+                    // assertion error
+                    //cy.get($list[i]).invoke('text').then(parseFloat).should("be.gt", 10)
+                }
+            })
+
+        })
+        
+
     })
 })
