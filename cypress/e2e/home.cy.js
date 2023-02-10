aea42762f817e6fad600c8872ece862d16ebbb10
Kristen-K <42575870+Kristen-K@users.noreply.github.com>
2023-02-10 19:27:07 +0000

    Added home page tests

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,34 @@
+
+describe('Home', () => {
+
+    beforeEach(() => {
+        cy.visit("https://practice.automationbro.com")
+    })
+    context("Home Page", () => {
+        it("open URL", () => {
+            cy.title().should("eq", "Practice E-Commerce Site – Automation Bro")
+        }) 
+    })
+   
+
+    context("About page", () => {
+        it("Open About page & assert test", () => {
+            cy.visit("https://practice.automationbro.com/about/")
+            cy.location("pathname").should("eq", "/about/")
+        })  
+
+    })
+
+    context("Additional Home page tests", () => {
+        it("Click get started button & assert URL contains get-started text", () => {
+            cy.get("#get-started").click()
+            cy.url("pathname").should("contain", "get-started")
+        })
+
+        it("Find heading element", () => {
+            cy.get(".elementor-widget-container h1").should("have.text", "Think different. Make different.")
+        })
+    })
+   
+   
+});
